42c1e3ea2d3f1aedb51f4166d505ac9b6f50c997
Sagheer Syed <sagheersyed333@gmail.com>
2022-05-02 09:35:32 +0500

    environment setup

--- android/app/src/main/java/com/todoapp/MainActivity.java
+++ android/app/src/main/java/com/todoapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.todoapp;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "todoapp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
